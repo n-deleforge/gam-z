a015442c40f8959e0e3ff0ba23e9802c0348b97d
n-deleforge <contact@nicolas-deleforge.fr>
2021-05-15 17:16:47 +0200

    New LittleJS

--- app/_footer.php
+++ app/_footer.php
@@ -1,7 +1,7 @@
         <?php echo ($_NAME == "home") ? '<div id="void"></div><div id="footer"></div>' : '<a href="index.php" class="gameButton" id="back"></a>'; ?>
     </div>
 
-    <script src="/../libraries/littleJS.min.js"></script>
+    <script src="/../libraries/littleJS.min.js?v=<?php echo  $_VERSION; ?>">'</script>
     <script src="assets/js/core.min.js?v=<?php echo  $_VERSION; ?>">'</script>
     <?php 
         if ($_NAME == "tictactoe")  echo '<script src="assets/js/tictactoe.min.js?v='. $_VERSION . '"></script>';
